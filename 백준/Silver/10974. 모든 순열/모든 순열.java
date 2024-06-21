379449cc2501f55972266cb248afbf9e21ade64a
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-06-21 10:18:21 +0900

    [Silver III] Title: 모든 순열, Time: 1888 ms, Memory: 51720 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N,C;
+	static int[] a,b;
+	static boolean[] v;
+	
+	static void perm(int cnt) {
+		if (cnt == N) {
+			 for (int i=0; i<N; i++) {
+	                System.out.print(b[i]+" ");
+	            }
+	            System.out.println();
+			return;
+		}
+		for (int i=0; i<N; i++) {
+			if(v[i]) continue;
+			v[i]=true;
+			b[cnt] = a[i];
+			perm(cnt+1);
+			v[i] = false;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		//1. 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		st = new StringTokenizer(br.readLine());
+				
+		N = Integer.parseInt(st.nextToken());
+		C = 0;
+		a = new int[N];
+		for (int i=1; i<=N; i++) {
+			a[i-1] = i;
+		}
+        //2. 순열
+		b = new int[N];
+		v = new boolean[N];
+		perm(0);
+	}
+
+}

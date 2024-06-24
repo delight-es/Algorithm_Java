f2595673f5acab7669cbdbd2f8f350405cbdebc7
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-06-24 16:08:38 +0900

    [Silver I] Title: 단지번호붙이기, Time: 144 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -12,9 +12,9 @@ public class Main {
 	static int[] di = {-1,0,1,0};
 	static int[] dj = {0,1,0,-1};
 	static int N, C, ALL_C; //배열 크기, 단지 개별개수, 전체개수
-	static int[][] map;
-	static boolean[][] v;
-	static ArrayList<Integer> len = new ArrayList<>();
+	static int[][] map; //지도
+	static boolean[][] v; //방문
+	static ArrayList<Integer> len = new ArrayList<>(); //개별 크기
 	
 	
 	//2. dfs
@@ -26,6 +26,27 @@ public class Main {
 			int nj = j + dj[d];
 			if (0<=ni && ni<N && 0<=nj && nj<N && !v[ni][nj] && map[ni][nj]==1) {
 				dfs(ni,nj);
+			}
+		}
+	}
+	
+	//3. bfs
+	static void bfs(int i, int j) {
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+		v[i][j] = true; //방문처리
+		q.offer(new int[] {i,j});
+		while(!q.isEmpty()) {
+			int[] ij = q.poll();
+			i = ij[0];
+			j = ij[1];
+			C++; //개수 카운트
+			for (int d=0; d<4; d++) {
+				int ni = i + di[d];
+				int nj = j + dj[d];
+				if (0<=ni && ni<N && 0<=nj && nj<N && !v[ni][nj] && map[ni][nj]==1) {
+					v[ni][nj] = true;
+					q.offer(new int[] {ni,nj});
+				}
 			}
 		}
 	}
@@ -42,21 +63,18 @@ public class Main {
 				map[i][j] = Character.getNumericValue(line.charAt(j));
 			}
 		}
-		//for(int[] m:map) System.out.println(Arrays.toString(m));
-		
-		//2. dfs
+		//2. dfs - 3. bfs
 		for (int x=0; x<N; x++) {
 			for (int y=0; y<N; y++) {
 				if(!v[x][y] && map[x][y]==1) {
-					dfs(x,y); //깊이우선탐색 -> 그룹 찾아서
+					//dfs(x,y); //2.깊이우선탐색 -> 그룹 찾아서
+					bfs(x,y); //3.너비우선탐색 -> 그룹 찾아서
 					len.add(C); //개별 개수 다 세면 추가
 					ALL_C++; //전체 개수+1
 					C=0; //다음 그룹 위해서 초기화
 				}
 			}
 		}
-		
-		//3. bfs
 		
 		//4. 출력
 		System.out.println(ALL_C); //전체 개수

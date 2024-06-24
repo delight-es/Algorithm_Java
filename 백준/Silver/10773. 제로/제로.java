f20b9f349657299b9f32ca6c7ce20bdfc9d46ca1
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-06-24 13:37:10 +0900

    [Silver IV] Title: 제로, Time: 236 ms, Memory: 23536 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,11 +1,17 @@
 import java.util.*;
 import java.io.*;
 
+/* 제로
+ * K : 입력될 숫자 수
+ * 0이면 가장 최근(마지막) 숫자 제거
+ * 아니면 다 더하기
+ * 맨 마지막거 제거해야하니 LIFO -> Stack
+ */
+
 public class Main {
 	public static void main(String[] args) throws Exception {
 		// 1. 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StreamTokenizer st = null;
 		int K = Integer.parseInt(br.readLine());
 		
 		//2. 스택 입력 및 처리

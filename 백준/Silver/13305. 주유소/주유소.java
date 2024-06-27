967cb5d8cbcbaa8aeabd2150f489c6444c633656
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-06-27 13:43:26 +0900

    [Silver III] Title: 주유소, Time: 316 ms, Memory: 30596 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+/* 주유소: 왼->오 최소비용 계산 문제
+ * N: 도시 개수, len: 도로길이배열
+ * price: 가격배열, total: 가격총합
+ * min: 최소가격, sum_len: 남은거리
+ * -분석: (첫가격*첫길이) + (최소가격*남은거리)
+ * -- 처음 기름 넣고 가야해서 첫출발가격 미리 계산
+ * -- 최소가격 계산시 마지막가격 무시 (남은거리=0)  
+ */
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		// 1. 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine()); //도시개수
+		int[] len = new int[N-1]; //길이배열
+		int[] price = new int[N]; // 가격배열
+		st = new StringTokenizer(br.readLine()," ");
+		for(int l=0; l<N-1; l++) len[l] = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine()," ");
+		for(int p=0; p<N; p++) price[p] = Integer.parseInt(st.nextToken());
+		
+		// 2. 처리
+		int total = len[0]*price[0]; //가격총합
+		int min=Integer.MAX_VALUE; 
+		for(int i=1; i<N-1; i++) min = Math.min(min, price[i]); //최소가격
+		int sum_len = 0;
+		for(int j=1; j<N-1; j++) sum_len += len[j]; //남은거리
+		total += sum_len*min;
+		
+		//3. 출력
+		System.out.println(total);
+		br.close();
+	}
+}

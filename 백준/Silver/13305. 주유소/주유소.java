e544aab7da594078ab6a69f15c4a617036347a4c
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-06-27 13:51:54 +0900

    [Silver III] Title: 주유소, Time: 332 ms, Memory: 30668 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -5,9 +5,9 @@ import java.util.*;
  * N: 도시 개수, len: 도로길이배열
  * price: 가격배열, total: 가격총합
  * min: 최소가격, sum_len: 남은거리
- * -분석: (첫가격*첫길이) + (최소가격*남은거리)
+ * -분석: (첫가격*첫길이) + 도시별(최소가격*길이)
  * -- 처음 기름 넣고 가야해서 첫출발가격 미리 계산
- * -- 최소가격 계산시 마지막가격 무시 (남은거리=0)  
+ * -- 최소가격 계산시 마지막가격 무시 (남은거리=0)
  */
 
 public class Main {
@@ -24,12 +24,12 @@ public class Main {
 		for(int p=0; p<N; p++) price[p] = Integer.parseInt(st.nextToken());
 		
 		// 2. 처리
-		int total = len[0]*price[0]; //가격총합
-		int min=Integer.MAX_VALUE; 
-		for(int i=1; i<N-1; i++) min = Math.min(min, price[i]); //최소가격
-		int sum_len = 0;
-		for(int j=1; j<N-1; j++) sum_len += len[j]; //남은거리
-		total += sum_len*min;
+		int total = len[0]*price[0]; //가격총합 초기화
+		int min = price[0]; //최소가격 초기화
+		for(int i=1; i<N-1; i++) {
+			min = Math.min(price[i], min); //갱신
+			total += (min*len[i]);
+		}
 		
 		//3. 출력
 		System.out.println(total);

c8ccca60a7a0afe31632858c07c4a687d227c902
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-07-03 17:16:16 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 496 ms, Memory: 57940 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -5,76 +5,51 @@ import java.io.*;
  * V:정점수, E:간선수,
  * (A,B,C):정점,정점,비용
  * 최소 스패닝 트리 가중치 --> 출력!
- * (분석) 크루스칼:최소 스패닝트리 = 최소신장트리
+ * (분석) 프림(우선순위큐):최소 스패닝트리 = 최소신장트리
  */
 
 public class Main {
-    static int V,E;
-	static int[] p;
-	static int[][] g;
-
-    //1.초기화
-	static void make() {
-		p = new int[V+1];
-		for(int i=1; i<=V; i++) p[i]=i;
-	}
-
-    //2.부모찾기
-	static int find(int a) {
-		if(p[a]==a) return a;
-		return p[a]=find(p[a]);
-	}
-
-    //3.합집합
-	static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if(aRoot == bRoot) return false;
-		p[bRoot] = aRoot;
-		return true;
-	}
-
-    public static void main(String[] args) throws Exception {
-        //1.입력
+	public static void main(String[] args) throws Exception{
+		//1. 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-
-        int[][] edges = new int[E][3]; // 간선 정보 배열
-
-        for(int i=0; i<E; i++) {
-            st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			edges[i] = new int[] {from,to,cost};
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		List<int[]>[] g = new List[V]; for(int i=0;i<V;i++) g[i]=new ArrayList<>();
+		for(int i=0; i<E; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			int from = Integer.parseInt(st.nextToken())-1;
+			int to = Integer.parseInt(st.nextToken())-1;
+			int c = Integer.parseInt(st.nextToken());
+			g[from].add(new int[] {to, c});
+			g[to].add(new int[] {from,c});
 		}
-
-        // 4. 간선을 가중치 기준으로 정렬
-        Arrays.sort(edges, (a,b) -> Integer.compare(a[2], b[2]));
-
-        // 5. Kruskal 알고리즘으로 최소 스패닝 트리 구하기
-        make();
-        int mstWeight = 0;
-        int edgeCount = 0;
-
-        for (int[] edge : edges) {
-            int from = edge[0];
-            int to = edge[1];
-            int cost = edge[2];
-
-            // 사이클을 형성하지 않는 경우에만 간선을 추가
-            if (union(from, to)) {
-                mstWeight += cost;
-                edgeCount++;
-                if (edgeCount == V - 1) break; // MST를 구성하는 간선의 개수가 V-1개일 때 종료
-            }
-        }
-
-        // 6. 결과 출력
-        System.out.println(mstWeight);
-
-        br.close();
-    }
+		
+		//2. 준비
+		boolean[] v = new boolean[V];
+		int[] w = new int[V];
+		for(int i=0; i<V; i++) w[i]=Integer.MAX_VALUE;
+		
+		//3. 프림 알고리즘 (우선순위 큐)
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)->Integer.compare(o1[1], o2[1]));
+		int sum=0, cnt=0;
+		w[0]=0;
+		pq.offer(new int[] {0,0});
+		while(!pq.isEmpty()) {
+			int[] vc = pq.poll();
+			int min = vc[1];
+			int minVertex = vc[0];
+			if(v[minVertex]) continue;
+			v[minVertex]=true;
+			sum+=min;
+			for(int[] j:g[minVertex]) {
+				if(!v[j[0]] && w[j[0]]>j[1]) {
+							   w[j[0]]=j[1];
+							   pq.offer(j);
+				}
+			}			
+		}
+		System.out.println(sum);
+		br.close();
+	}
 }

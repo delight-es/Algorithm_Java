9c66f0b6758e1403a01fe927ad7a6272a6eaa583
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-07-10 10:14:13 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -2,11 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-	//반복문 2개 --> 수행시간: n^2
+	//반복문 2개지만, --> n*(n-1)*(n-2)*1 = n*(n-1)/2
 	/* static int MenOfPassion(int[] A, int n) {
 		int sum = 0;
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=n; j++) {
+		for(int i=1; i<=n-1; i++) { //1, 2, 3 
+			for(int j=i+1; j<=n; j++) { //2~n, 3~n, 4~n
 				sum += A[i];
 			}
 		}

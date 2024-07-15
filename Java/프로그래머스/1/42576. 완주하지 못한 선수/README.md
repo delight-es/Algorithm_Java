a062d9d949b71af63fdd1d7a2ec2f884f1854d97
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-07-15 14:07:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.00 ms, Memory: 96.5 MB -BaekjoonHub

--- Java/프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java#) 
 
 ### 성능 요약
 
-메모리: 111 MB, 시간: 278.56 ms
+메모리: 96.5 MB, 시간: 73.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 15일 13:37:21
+2024년 07월 15일 14:07:22
 
 ### 문제 설명
 
--- Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,27 +4,37 @@ import java.util.*;
 /* 완주하지 못한 선수
  * participant: 마라톤 참여 명단
  * completion: 완주 명단
- * (분석) 정렬<명단순서 맞춰주기> -> 비교<없는이름 확인>
+ * (분석 1) 정렬[명단순서 맞춰주기] -> 비교[없는이름 확인]
+ * (분석 2) 해시[이름,인원수] -> 비교[완주명단 가지고 해시-1씩] -> 최종 남은 해시 반환
  */
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-         //(1) 정렬 (명단순서 맞추기)
-		 Arrays.sort(participant);
-		 Arrays.sort(completion);
-
-		 //(2) 없는 명단 확인
-		 for(int i=0; i<completion.length; i++) {
-			 if(!participant[i].equals(completion[i])) { //다르면
-				 answer = participant[i]; //없는 친구로 간주
-                 break;
-			 }
-		 }
+        
+		 //(1) 해시 [이름,인원수]
+         HashMap<String, Integer> h = new HashMap<>();
+         for(String p:participant) {
+             //put: 해시(키,값) 추가
+             //getOrDefault(키,디폴트): 키값 반환 (없음 디폴트로)
+             h.put(p, h.getOrDefault(p,0)+1);
+         }
+        
+         //(2) 비교 [완주명단 가지고 해시-1씩]
+         for(String c:completion) {
+             //replace(키,값): 키값 수정
+             //get(키): 키값 반환
+             h.replace(c,h.get(c)-1);
+         }
+        
+         //(3) 값!=0인 키 반환
+        for(String p:participant) {
+            if(h.get(p)!=0) {
+                answer=p; break;
+            }
+        }
         
 		 //(3) 반환
-         if(answer == "") //완주명단 다 돌았는데 없으면
-             answer = participant[participant.length-1]; //마지막 참가자
 	     return answer;
     }
 }

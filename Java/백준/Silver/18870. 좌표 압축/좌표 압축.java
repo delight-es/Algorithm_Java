de0e3523649e89d7a72bffb6d41ce9f35840fba5
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-07-19 17:51:29 +0900

    [Silver II] Title: 좌표 압축, Time: 2008 ms, Memory: 292856 KB -BaekjoonHub

--- Java/백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ Java/백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -8,20 +8,22 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		  //(1) 받을배열
-		int[] arr = new int[N];
-		for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
-		  //(2) 중복제거 set
-		Set<Integer> s = new TreeSet<>();
-		for(int a:arr) s.add(a);
-		  //(3) 인덱스,값 map
-		Map<Integer, Integer> m = new HashMap<>();
-		int idx = 0;
-		for(int ss:s) m.put(ss,idx++);
-		  //(4) 출력
+		
+		//2. 처리
+		  // (1) 기존배열, 정렬배열
+		int[] arr=new int[N], sort=new int[N];
+		for(int i=0; i<N; i++) sort[i]=arr[i]=Integer.parseInt(st.nextToken());
+		Arrays.sort(sort);
+		  // (2) 중복제거,(값/인덱스)저장 해시맵
+		HashMap<Integer, Integer> m = new HashMap<>();
+		int rank=0;
+		for(int s:sort) {
+			if(!m.containsKey(s)) m.put(s,rank++);
+		}
+		
+		//3. 출력
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<N; i++) sb.append(m.get(arr[i])+" ");
+		for(int a:arr) sb.append(m.get(a)+" ");
 		System.out.println(sb.toString());
-		br.close();
 	}
 }

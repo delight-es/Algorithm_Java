d4c0b8b0134a317b0fbb5bbbb258eeabdff81b2e
eunsoo <159528848+delight-es@users.noreply.github.com>
2024-08-11 18:22:59 +0900

    [level 3] Title: 여행경로, Time: 85.60 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -12,21 +12,21 @@ import java.util.*;
 class Solution {
     
     static String[][] ticket; //항공권
-	static boolean[] v; //항공권 - 사용여부
-	static List<String> pathList; //경로리스트
+	static boolean[] v; //항공권 사용여부
+	static List<String> pathList; //경로 리스트
 	
-	static void dfs(int i, String start, String path) {
-		if(i == ticket.length) { //종료조건
+	static void dfs(int i, String start, String path) { //count, 출발지, 경로
+		if(i == ticket.length) { //종료 조건
 			pathList.add(path); //경로 저장
 			return;
 		}
 		
-		for(int d=0; d<ticket.length; d++) { //인접 경로 검색
+		for(int d=0; d<ticket.length; d++) { //인접 가능 경로 검색
 			String end = ticket[d][1];
-			if(ticket[d][0].equals(start) && !v[d]) { //현재도착=다음출발, 방문X
-				v[d]=true;
-				dfs(i+1, end, path+" "+end);
-				v[d]=false;
+			if(ticket[d][0].equals(start) && !v[d]) { //현재도착 인덱스(d) 찾기 (방문X)
+				v[d]=true; //방문
+				dfs(i+1, end, path+" "+end); //깊이+1, 다음도착지, 경로추가
+				v[d]=false; //방문해제 (백트래킹)
 			}
 		}
 	}
